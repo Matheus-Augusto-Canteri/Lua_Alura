a3dd1d2433c0ab1cc04892c335e889437fa2b80a
RoronoaTheuzo <101112856+RoronoaTheuzo@users.noreply.github.com>
2022-06-23 09:59:44 -0300

    Testes

---
+++ Testes.lua
@@ -0,0 +1,24 @@
+ano = 2018
+type(ano)
+
+nome  = "Fabio"
+type(nome)
+nome = string.reverse(nome)
+print(nome)
+
+function imprime()
+    print("Matheus")
+end
+
+imprime()
+
+imprimeMsg = function() -- Anonima
+    print("Canteri")
+end
+
+imprimeMsg()
+
+nomes = {"Ana", "Carlos", "Joao"}
+#nomes
+
+nomes[2]
